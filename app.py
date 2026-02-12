58019f5358ffdf617e1a515d720c9dd7d5a3e2e9
Abhikrit Patel <abhikritpatel05@gmail.com>
2026-02-13 03:24:41 +0530

    feat:successfully implemented POST endpoint for live data updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 import json
+from datetime import datetime
 
 app=Flask(__name__)
 app.json.sort_keys=False
@@ -23,6 +24,44 @@ def get_sorted_leaderboard():
     except Exception as e:
         print(f"Error during sort: {e}")
         return {}
+
+@app.route('/api/add_win',methods=['POST'])
+def add_win():
+    data=request.get_json()
+
+    name=data.get('name','').lower()
+    amount=int(data.get('winning',0))
+    today=datetime.now().strftime('%Y-%m-%d')
+
+    if not isinstance(amount,(int,float)):
+        return jsonify({"status":"error","message":"Winning must be a number"}),400
+
+    try:
+        with open('data.json','r') as f:
+            players=json.load(f)
+        
+        if name in players:
+            players[name]['total_winning']+=amount
+            players[name]['history'].append(
+                {
+                    'date':today,
+                    'winning':amount
+                    
+                }
+            )
+            with open('data.json','w') as f:
+                json.dump(players,f,indent=2)
+
+            return jsonify({"status":"success", "message": f'{amount} added to {name}'}),200
+    
+        return jsonify({"status":"error","message":"player not found"}),404
+    
+    except Exception as e:
+        return jsonify({"status":"error","message":str(e)}),500
+    
+
+
+
 
 
 @app.route('/api/leaderboard',methods=['GET'])
